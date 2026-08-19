67862709c13ac99f8090b64b88438964d2275fc3
gennadijrybanec-hash <gennadijrybanec@gmail.com>
2026-08-19 13:50:47 +0300

    Update MainActivity.kt

--- app/src/main/java/com/stitchcraft/app/MainActivity.kt
+++ app/src/main/java/com/stitchcraft/app/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
@@ -372,6 +373,7 @@ fun PatternCanvas(
     Canvas(
         modifier
             .background(Color.White)
+            .clipToBounds()
             .pointerInput(pattern.width, pattern.height, scale) {
                 detectTapGestures { offset ->
                     val cellSize = minOf(
