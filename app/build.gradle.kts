bea9d40a9f18d92792147461aefb26c379944323
gennadijrybanec-hash <gennadijrybanec@gmail.com>
2026-08-27 12:35:34 +0200

    StitchCraft RC8 Billing Fix 2 - versionCode 115

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.stitchcraft.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 114
-        versionName = "1.0.0-rc8-billing-test"
+        versionCode = 115
+        versionName = "1.0.0-rc8-billing-fix2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
